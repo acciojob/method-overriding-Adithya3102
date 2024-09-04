c28e97da700f43cd97a0f944e9dc1efffe3ae17f
KAdi <124163172+Adithya3102@users.noreply.github.com>
2024-09-04 22:29:20 +0530

    Method Overriding

---
+++ ANOTHER.java
@@ -0,0 +1,22 @@
+class A {
+    public String meth() {
+        return "Invoking method from class A";
+    }
+}
+
+class B extends A {
+    @Override
+    public String meth() {
+        return "Method is overridden in Extended class B";
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        B obj = new B();
+
+       
+        String result1 = obj.meth();
+        System.out.println(result1);
+    }
+}
